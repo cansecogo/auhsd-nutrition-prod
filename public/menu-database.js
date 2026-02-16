d2198d31749820f04b24d1a9ba632fd9b5a956a9
cansecogo <73351720+cansecogo@users.noreply.github.com>
2026-02-15 20:02:27 -0800

    updated full code

---
+++ public/menu-database.js
@@ -0,0 +1,603 @@
+const MENU_DATABASE = {
+  "high_school": {
+    "breakfast": [
+      {
+        "item": "Apple Frudel",
+        "recipe": "112999.0",
+        "serving": "1 each",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Bagel w/ Cream Cheese",
+        "recipe": "14587.0",
+        "serving": "1 serving",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Banana Bread",
+        "recipe": "149384.0",
+        "serving": "1 each",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Benefit Bars - Chocolate",
+        "recipe": "139395.0",
+        "serving": "1 each",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Benefit Bars- Chocolate Chip",
+        "recipe": "113232.0",
+        "serving": "1 each",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Berry Berry Muffin",
+        "recipe": "1568283.0",
+        "serving": "1 serving",
+        "contribution": "2 Grain, 1/8c. Fruit"
+      },
+      {
+        "item": "Breakfast Bar: Pancakes",
+        "recipe": "1206951.0",
+        "serving": "1 serving",
+        "contribution": "1 Grain, 1 Meat, 1/4c.Veg."
+      },
+      {
+        "item": "Breakfast Bowl, Turkey Chorizo w Pico de Gallo",
+        "recipe": "1206647.0",
+        "serving": "1 serving",
+        "contribution": "1WG,2M/MA,1/2 c. Veg"
+      },
+      {
+        "item": "Breakfast Bowl, Eggstravaganza w Pico de Gallo",
+        "recipe": "1206793.0",
+        "serving": "1 serving",
+        "contribution": "2 Grain, 1.75 Meat, 1/2c. Veg"
+      },
+      {
+        "item": "Breakfast Burrito (Egg, Cheese, Bacon) IW",
+        "recipe": "405275.0",
+        "serving": "1 each",
+        "contribution": "1.5 Grain, 1.25 Meat"
+      },
+      {
+        "item": "Breakfast Burrito w/ Salsa",
+        "recipe": "1361361.0",
+        "serving": "1 serving",
+        "contribution": "2 Grain, 2.5 Meat, 1/4c Veg"
+      },
+      {
+        "item": "Breakfast Croissant Sandwich (Sausage)",
+        "recipe": "14562.0",
+        "serving": "1 serving",
+        "contribution": "1.5 Grain, 2.75 Meat"
+      },
+      {
+        "item": "Breakfast English Muffin Sandwich (Sausage)",
+        "recipe": "14562.0",
+        "serving": "1 serving",
+        "contribution": "2 Grain, 3 Meat"
+      },
+      {
+        "item": "Breakfast Pan Bread- Cowgirl",
+        "recipe": "134323.0",
+        "serving": "1 serving",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Breakfast Pan Bread- Lemon",
+        "recipe": "140057.0",
+        "serving": "1 serving",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Breakfast Pan Bread- Pumpkin",
+        "recipe": "138894.0",
+        "serving": "1 serving",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Cinn Crumb Square",
+        "recipe": "113034.0",
+        "serving": "1 each",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Cinnamon Roll",
+        "recipe": "141214/ 133740",
+        "serving": "1 serving",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Concha - Assorted",
+        "recipe": "133841.0",
+        "serving": "1 each",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Concha - Pink",
+        "recipe": "133843.0",
+        "serving": "1 each",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "French Toast Sticks - Bulk",
+        "recipe": "139402.0",
+        "serving": "1 serving",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "French Toast Sticks- IW",
+        "recipe": "113685.0",
+        "serving": "1 serving",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Frosted Donut - Chocolate",
+        "recipe": "263501.0",
+        "serving": "1 serving",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Frosted Donut - Vanilla",
+        "recipe": "263500.0",
+        "serving": "1 serving",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Fruit Yogurt Parfait",
+        "recipe": "269133, 14635",
+        "serving": "1 serving",
+        "contribution": "1 Grain, 1 Meat, 1/2c. Fruit"
+      },
+      {
+        "item": "Garlic Cheese Toast - Bulk",
+        "recipe": "154403.0",
+        "serving": "1 each",
+        "contribution": "2 Grain, 2 Meat"
+      },
+      {
+        "item": "Garlic Cheese Toast-IW",
+        "recipe": "404501.0",
+        "serving": "1 each",
+        "contribution": "2 Grain, 2 Meat"
+      },
+      {
+        "item": "Lemon Blueberry Muffin",
+        "recipe": "1372635.0",
+        "serving": "1 serving",
+        "contribution": "2 Grain, 1/8c. Fruit"
+      },
+      {
+        "item": "Maple Butter Waffle",
+        "recipe": "141470.0",
+        "serving": "1 each",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Mini Bagels, Strawberry",
+        "recipe": "134813.0",
+        "serving": "1 each",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Mini Cinn Rolls",
+        "recipe": "113361.0",
+        "serving": "1 serving",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Mini Donuts- Chocolate",
+        "recipe": "139662.0",
+        "serving": "1 serving",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Mini Donuts- Powdered Sugar",
+        "recipe": "139661.0",
+        "serving": "1 serving",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Mini French Toast, Chocolate Chip",
+        "recipe": "134627.0",
+        "serving": "1 serving",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Mini Pancakes",
+        "recipe": "134287.0",
+        "serving": "1 serving",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Mini Waffles, Blueberry",
+        "recipe": "134267.0",
+        "serving": "1 serving",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Muffin - Chocolate Chip",
+        "recipe": "406578.0",
+        "serving": "1 each",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Muffin: Apple Cinnamon",
+        "recipe": "400014.0",
+        "serving": "1 each",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Muffin: Banana",
+        "recipe": "400020.0",
+        "serving": "1 each",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Muffin: Blueberry",
+        "recipe": "400016.0",
+        "serving": "1 each",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Pancake Wrapped Sausage",
+        "recipe": "100760.0",
+        "serving": "1 each",
+        "contribution": "1 Grain, 1 Meat"
+      },
+      {
+        "item": "Smoothie Bowl",
+        "recipe": "1325360.0",
+        "serving": "1 serving",
+        "contribution": "1 Grain, 2 Meat, 1c Fruit"
+      },
+      {
+        "item": "Smoothie w/ Graham Cracker",
+        "recipe": "14667, 14538, 269132, 14785",
+        "serving": "1 serving",
+        "contribution": "1 Grain, 1.25 Meat,  1/2c Fruit"
+      },
+      {
+        "item": "Stuffed Cinn French Toast Bar",
+        "recipe": "141180.0",
+        "serving": "1 each",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Toasted Ham Cheese Sandwich (DFC)",
+        "recipe": "DFC8074/\n14697",
+        "serving": "1 serving",
+        "contribution": "2 Grain, 2 Meat"
+      },
+      {
+        "item": "Waffles (bulk)",
+        "recipe": "138923.0",
+        "serving": "1 serving",
+        "contribution": "2 Grain"
+      },
+      {
+        "item": "Yogurt (4 oz.) w/ Crackers",
+        "recipe": "14660, 14646, 399978",
+        "serving": "1 serving",
+        "contribution": "1 Grain, 1 Meat"
+      },
+      {
+        "item": "Zucchini Bread",
+        "recipe": "149390.0",
+        "serving": "1 each",
+        "contribution": "2 Grain"
+      }
+    ],
+    "lunch": [],
+    "salad_bar": [
+      {
+        "item": "1911",
+        "item_number": "Extra",
+        "serving": "CILANTRO 6EA\nCilantro = 1 Tbsp"
+      },
+      {
+        "item": "7127",
+        "item_number": "Extra",
+        "serving": "CILANTRO-CHOPPED 1 LB\nCilantro = 1 Tbsp"
+      },
+      {
+        "item": "209918",
+        "item_number": "Extra",
+        "serving": "JALAPENOS, CANNED\nJalapenos, Canned = 1 oz."
+      },
+      {
+        "item": "253496",
+        "item_number": "Extra",
+        "serving": "PICKLES\nPickles = 1 oz. (9 pieces)"
+      },
+      {
+        "item": "2647",
+        "item_number": "Fruit",
+        "serving": "APPLE-GRANNY SMITH XF 138CT, 40lb\nApples, Whole = 1 cup Fruit"
+      },
+      {
+        "item": "9203",
+        "item_number": "Fruit",
+        "serving": "APPLE-RED (VARIETY) 138CT, 40lb\nApples, Whole = 1 cup Fruit"
+      },
+      {
+        "item": "2718",
+        "item_number": "Fruit",
+        "serving": "BANANA-PETITE GREEN TIP 40LB 150 COUNT\nBanana, Petite = 1/2 cup Fruit"
+      },
+      {
+        "item": "2719",
+        "item_number": "Fruit",
+        "serving": "BANANA-PETITE RIPE 40LB 150 COUNT\nBanana, Petite = 1/2 cup Fruit"
+      },
+      {
+        "item": "10441",
+        "item_number": "Fruit",
+        "serving": "=D10&\"\n\"&C10"
+      },
+      {
+        "item": "8534",
+        "item_number": "Fruit",
+        "serving": "CARA CARA 56/72SZ , 20LB\nOranges = 1/2 cup Fruit"
+      },
+      {
+        "item": "406948",
+        "item_number": "Fruit",
+        "serving": "CMDY APPLESAUCE UNSWT CINNAMON\nApplesauce, 4.5 oz = 1/2 cup Fruit"
+      },
+      {
+        "item": "406947",
+        "item_number": "Fruit",
+        "serving": "CMDY APPLESAUCE UNSWT PLAIN\nApplesauce, 4.5 oz = 1/2 cup Fruit"
+      },
+      {
+        "item": "409797",
+        "item_number": "Fruit",
+        "serving": "CMDY APPLESAUCE UNSWT STRWBRY\nApplesauce, 4.5 oz = 1/2 cup Fruit"
+      },
+      {
+        "item": "10161",
+        "item_number": "Fruit",
+        "serving": "CMDY-APPLE-SLICED F.I 100/2 OZ\nApples, Whole = 1 cup Fruit"
+      },
+      {
+        "item": "203165",
+        "item_number": "Fruit",
+        "serving": "CRAISINS, CHERRY FLAVOR, 1.16 OZ\nDried Cranberries = 1/2 cup Fruit"
+      },
+      {
+        "item": "208210",
+        "item_number": "Fruit",
+        "serving": "CRAISINS, ORANGE FLAVOR, 1.16 OZ\nDried Cranberries = 1/2 cup Fruit"
+      },
+      {
+        "item": "203180",
+        "item_number": "Fruit",
+        "serving": "CRAISINS, STRAWBERRY FLAVOR, 1.16 OZ\nDried Cranberries = 1/2 cup Fruit"
+      },
+      {
+        "item": "239300",
+        "item_number": "Fruit",
+        "serving": "CRAISINS, WATERMELON FLAVOR, 1.16 OZ\nDried Cranberries = 1/2 cup Fruit"
+      },
+      {
+        "item": "500138",
+        "item_number": "Fruit",
+        "serving": "FROZEN STRAWBERRY CUP 4.5OZ\nStrawberry = 1/2 cup Fruit"
+      },
+      {
+        "item": "6418",
+        "item_number": "Fruit",
+        "serving": "GRAPES - IW , 100 COUNT/3OZ\nGrapes = 1/2 cup Fruit"
+      },
+      {
+        "item": "2745",
+        "item_number": "Fruit",
+        "serving": "GRAPES-RED LUNCH BUNCH 21 LBS\nGrapes = 1/2 cup Fruit"
+      },
+      {
+        "item": "13402",
+        "item_number": "Fruit",
+        "serving": "=D23&\"\n\"&C23"
+      },
+      {
+        "item": "10442",
+        "item_number": "Fruit",
+        "serving": "=D24&\"\n\"&C24"
+      },
+      {
+        "item": "500567",
+        "item_number": "Fruit",
+        "serving": "IQF BLUEBERRIES, FROZEN\nBlueberries = 1/2 cup Fruit"
+      },
+      {
+        "item": "140163",
+        "item_number": "Fruit",
+        "serving": "IQF BLUEBERRIES, FROZEN\nBlueberries = 1/2 cup Fruit"
+      },
+      {
+        "item": "500591",
+        "item_number": "Fruit",
+        "serving": "IQF SLICED STRAWBERRIES, FROZEN\nStrawberry = 1/2 cup Fruit"
+      },
+      {
+        "item": "2756",
+        "item_number": "Fruit",
+        "serving": "KIWI-BULK 24 LBS 109 COUNT (available starting September)\nKiwi = 1/2 cup Fruit"
+      },
+      {
+        "item": "1969",
+        "item_number": "Fruit",
+        "serving": "=D29&\"\n\"&C29"
+      },
+      {
+        "item": "2765",
+        "item_number": "Fruit",
+        "serving": "MELON-CANTALOUPE (Approx 1.8 lb melon)\nCantaloupe = 1/2 cup Fruit"
+      },
+      {
+        "item": "2779",
+        "item_number": "Fruit",
+        "serving": "MELON-HONEYDEW (approx. 2.2 lb melon)\nHoneydew = 1/2 cup Fruit"
+      },
+      {
+        "item": "248565",
+        "item_number": "Fruit",
+        "serving": "MIXED FRUIT CAN\nMixed Fruit = 1/2 cup Fruit"
+      },
+      {
+        "item": "2789",
+        "item_number": "Fruit",
+        "serving": "NECTARINE (VARIETY) V/F 25 LBS 80-90 COUNT\nNectarine = 1/2 cup Fruit"
+      },
+      {
+        "item": "10279",
+        "item_number": "Fruit",
+        "serving": "=D34&\"\n\"&C34"
+      },
+      {
+        "item": "2131",
+        "item_number": "Fruit",
+        "serving": "ORANGE-CHOICE 138CT, 40lb\nOranges = 1/2 cup Fruit"
+      },
+      {
+        "item": "2811",
+        "item_number": "Fruit",
+        "serving": "PEACH (VARIETY) V/F 25 LBS 80-90 COUNT\nPeaches = 1/2 cup Fruit"
+      },
+      {
+        "item": "500136",
+        "item_number": "Fruit",
+        "serving": "PEACH CUPS FROZEN\nPeach Cup, Chilled = 1/2 cup Fruit"
+      },
+      {
+        "item": "500134",
+        "item_number": "Fruit",
+        "serving": "PEACHES DICED CAN\nPeaches, Diced, Canned = 1/2 cup Fruit"
+      },
+      {
+        "item": "248372",
+        "item_number": "Fruit",
+        "serving": "PEACHES DICED CAN\nPeaches, Diced, Canned = 1/2 cup Fruit"
+      },
+      {
+        "item": "6382",
+        "item_number": "Fruit",
+        "serving": "PEAR-(VARIETY) 120/135CT, 40 LB\nPears = 1/2 cup Fruit"
+      },
+      {
+        "item": "6382",
+        "item_number": "Fruit",
+        "serving": "PEAR-(VARIETY) 120/135CT, 40LB\nPears = 1/2 cup Fruit"
+      },
+      {
+        "item": "22689",
+        "item_number": "Fruit",
+        "serving": "=D42&\"\n\"&C42"
+      },
+      {
+        "item": "500150",
+        "item_number": "Fruit",
+        "serving": "PEARS DICED CAN\nPears, Diced Canned = 1/2 cup Fruit"
+      },
+      {
+        "item": "10818",
+        "item_number": "Fruit",
+        "serving": "PERSIMMONS 110-125/cs, 25LB\nPersimmon = 1/2 cup Fruit"
+      },
+      {
+        "item": "9296",
+        "item_number": "Fruit",
+        "serving": "=D45&\"\n\"&C45"
+      },
+      {
+        "item": "113223",
+        "item_number": "Fruit",
+        "serving": "PINEAPPLE CHUNKS 4/5# \nPineapple = 1/2 cup Fruit"
+      },
+      {
+        "item": "209856",
+        "item_number": "Fruit",
+        "serving": "PINEAPPLE TIDBITS IN JUICE\nPineapple Tidbits, Canned = 1/2 cup Fruit"
+      },
+      {
+        "item": "10440",
+        "item_number": "Fruit",
+        "serving": "PINEAPPLE-CHUNKS 2/5 LBS\nPineapple = 1/2 cup Fruit"
+      },
+      {
+        "item": "10352",
+        "item_number": "Fruit",
+        "serving": "PINEAPPLE-GOLD 7CT\nPineapple = 1/2 cup Fruit"
+      }
+    ]
+  },
+  "elementary": {
+    "breakfast": [
+      "Crackers, Goldfish - 1 Grain",
+      "Bar, Oatmeal Chocolate Round",
+      "Bagel, Mini Strawberry",
+      "String Cheese",
+      "1.25 Grain, 1M/MA",
+      "2 Grain, 1.5 M/MA",
+      "Cereal, Variety  w/ Crackers- 2 Grain",
+      "Cereal, Cinnamon Toast Crunch, Reduced Sugar",
+      "Crackers, Goldfish Pretzel - 1 Grain",
+      "Crackers, Goldfish",
+      "Crackers. Scooby Doo - 1 Grain",
+      "Yogurt, Peach, 4 oz",
+      "Cereal, Variety",
+      "Muffin, Breakfast Chip",
+      "Cereal, Froot Loops, Reduced Sugar  w/ Crackers- 2 Grain",
+      "Crackers, Cinnamon Belly Bears",
+      "Waffle, Maple Butter",
+      "1 M/MA",
+      "Bread, Pumpkin",
+      "Yogurt, Strawberry, 4 oz",
+      "Crackers, Jungle",
+      "Bread, Concha",
+      "Sandwich, Turkey & Cheese Muffin",
+      "Yogurt, Trix, 4 oz",
+      "Bagel, Pizza",
+      "Crackers, Cinnamon Belly Bears - 1 Grain",
+      "Yogurt, Strawberry Banana, 4 oz",
+      "Bar, Benefit, Cocoa Chip",
+      "Cereal, Rice Chex, Plain  w/ Crackers- 2 Grain",
+      "Cereal, Rice Chex, Cinnamon w/ Crackers- 2 Grain"
+    ],
+    "lunch": [
+      "Cereal, Lucky Charms",
+      "French Toast Sticks",
+      "Crackers, Vanilla Bear Grahams",
+      "1 Grain",
+      "Cereal, Apple Jacks, Reduced Sugar",
+      "Muffin, Blueberry",
+      "Cereal, Rice Chex, Blueberry",
+      "Pancake, Mini Maple",
+      "Crackers, Tiger Bites",
+      "Cereal, Cheerios, Plain",
+      "Cereal, Cinnamon Toast Crunch, Reduced Sugar w/ Crackers- 2 Grain",
+      "Crackers, Cheez-It",
+      "Cereal, Cocoa Puffs, Reduced Sugar",
+      "Crackers, Jungle - 1 Grain",
+      "Crackers, Tiger Bites - 1 Grain",
+      "Quesadilla",
+      "Crackers, Chocolate Bear Graham - 1 Grain",
+      "Cereal, Froot Loops, Reduced Sugar",
+      "Granola, Strawberry",
+      "Bread, Banana",
+      "Crackers, Goldfish Pretzel",
+      "Cereal, Rice Chex, Cinnamon",
+      "Muffin, Banana",
+      "Bread, Zucchini",
+      "Colby Jack Cheese",
+      "Cereal, Cheerios, Plain  w/ Crackers- 2 Grain",
+      "Crackers, Vanilla Bear Grahams - 1 Grain",
+      "Cereal, Lucky Charms  w/ Crackers- 2 Grain",
+      "Cereal, Rice Chex, Plain",
+      "Cereal, Cocoa Puffs, Reduced Sugar w/ Crackers- 2 Grain"
+    ]
+  }
+};
